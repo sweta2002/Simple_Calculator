ca52e24ca166ac7b86ff53cc63c3d90b26f530d6
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-18 23:33:22 +0530

    defining addition and subtraction

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 def addition(number_1, number_2):
+    add = number_1 + number_2
+    print("sum is ",add)
 
 def subtraction(number_1, number_2):
+    sub = number_1 - number_2
+    print("difference is ",sub)
 
 def multiplication(number_1, number_2):
 
@@ -8,4 +12,3 @@ def division(number_1, number_2):
 
 def get_input():
 
-input()
